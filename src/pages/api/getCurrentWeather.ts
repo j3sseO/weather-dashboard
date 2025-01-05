a7e64890e781ce750050e22b1d3e681fb25b8ccc
Jesse OConnor <jesseoconnor000@gmail.com>
2025-01-05 19:57:41 +1300

    Get weather + location data from Tomorrow.io and OpenStreetMap rather than OpenWeatherMap

--- src/pages/api/getCurrentWeather.ts
+++ src/pages/api/getCurrentWeather.ts
@@ -4,27 +4,58 @@ import type { APIRoute } from 'astro'
 export const prerender = false
 
 export const POST: APIRoute = async ({ request }) => {
-  const OPEN_WEATHER_API_KEY = import.meta.env.OPEN_WEATHER_API_KEY
-
   try {
     const data = await request.json()
     const { lat, lon } = data
 
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
-    const response = await fetch(weatherUrl)
-    const weatherData = await response.json()
+    const [weatherData, locationData] = await Promise.all([
+      getWeatherData(lat, lon),
+      getLocationDetails(lat, lon),
+    ])
 
-    return new Response(JSON.stringify(weatherData), {
+    return new Response(JSON.stringify({ weatherData, locationData }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     })
   } catch (error) {
+    console.error('Error fetching weather/location data:', error)
     return new Response(
-      JSON.stringify({ error: 'Failed to fetch weather data' }),
+      JSON.stringify({ error: 'Failed to fetch weather/location data' }),
       {
         status: 500,
         headers: { 'Content-Type': 'application/json' },
       },
     )
   }
+}
+
+export const getWeatherData = async (lat: number, lon: number) => {
+  const TOMORROW_API_KEY = import.meta.env.TOMORROW_API_KEY
+
+  // Get current weather data
+  const currentResponse = await fetch(
+    `https://api.tomorrow.io/v4/weather/realtime?location=${lat},${lon}&units=metric&apikey=${TOMORROW_API_KEY}`,
+  )
+  const currentData = await currentResponse.json()
+
+  // Get forecasted weather data
+  const weatherResponse = await fetch(
+    `https://api.tomorrow.io/v4/weather/forecast?location=${lat},${lon}&timesteps=1h,1d&apikey=${TOMORROW_API_KEY}`,
+  )
+  const weatherData = await weatherResponse.json()
+
+  return { currentData, weatherData }
+}
+
+export const getLocationDetails = async (lat: number, lon: number) => {
+  // Get location details from OpenStreetMap
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`,
+  )
+
+  if (!response.ok)
+    throw new Error(`Geocoding API Error: ${response.statusText}`)
+
+  const locationData = await response.json()
+  return locationData
 }
