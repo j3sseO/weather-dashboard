a4da01d2499ddc15b65cc867659809ca7604d845
Jesse OConnor <jesseoconnor000@gmail.com>
2025-02-12 22:42:12 +1300

    Install netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: netlify(),
 })
