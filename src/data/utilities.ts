119f68f95b7d784ae9818aee6149b66897062a45
Jesse OConnor <jesseoconnor000@gmail.com>
2025-02-14 22:11:21 +1300

    Replace temp unit dropdown with buttons

--- src/data/utilities.ts
+++ src/data/utilities.ts
@@ -1,3 +1,10 @@
-export function celsiusToFahrenheit(celsius: number): number {
-  return Math.round((celsius * 9) / 5 + 32)
+export const celsiusToFahrenheit = (celsius: number): number =>
+  Math.round((celsius * 9) / 5 + 32)
+
+export const setActiveButton = (
+  activeButton: HTMLElement,
+  inactiveButton: HTMLElement,
+) => {
+  activeButton.classList.add('bg-white/10')
+  inactiveButton.classList.remove('bg-white/10')
 }
