5aa676d60d112b55a14821a41607969625c3ce78
Jesse OConnor <jesseoconnor000@gmail.com>
2025-02-14 22:25:03 +1300

    Add precipitation view to data graph

--- src/data/utilities.ts
+++ src/data/utilities.ts
@@ -2,9 +2,9 @@ export const celsiusToFahrenheit = (celsius: number): number =>
   Math.round((celsius * 9) / 5 + 32)
 
 export const setActiveButton = (
-  activeButton: HTMLElement,
-  inactiveButton: HTMLElement,
+  activeButton: HTMLElement[],
+  inactiveButtons: HTMLElement[],
 ) => {
-  activeButton.classList.add('bg-white/10')
-  inactiveButton.classList.remove('bg-white/10')
+  activeButton.forEach(button => button.classList.add('bg-white/10'))
+  inactiveButtons.forEach(button => button.classList.remove('bg-white/10'))
 }
